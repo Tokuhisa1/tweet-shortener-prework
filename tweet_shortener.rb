7712787b72794333f528a43f57632ddc33a4abbf
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-12 20:26:46 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+def dictionary
+  {
+    hello: 'hi',
+    to: '2',
+    two: '2',
+    too: '2',
+    for: '4',
+    four: '4',
+    be: 'b',
+    you: 'u',
+    at: '@',
+    and: '&'
+  }
+end
+
+def word_substituter(string) # Takes in string as input
+  # Create variable to store output array
+  finish = []
+  # Create a counter variable
+  counter = 0
+  # Split string into an array
+  start = string.split
+  # Compare words to dictionary
+  while counter < start.length
+    # If word in dictionary, replace with abbr.
+    finish.push(dictionary.has_key?(start[counter].to_sym) ?
+                                                    # Else push word
+                  dictionary[start[counter].to_sym] : start[counter])
+    counter += 1
+  end
+  # Returns shorter string as output
+  finish.join(' ')
+end
